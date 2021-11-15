02a2f7d00a17b0c01dfc6ee0be89e851fe4e6eb8
JenyferRico <jenniferico2001@gmail.com>
2021-11-15 03:11:36 -0500

    Primeros cambios para Front

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
+import {Auth0Provider} from '@auth0/auth0-react';
+//import { Routes } from 'react-router';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Routes from './Routes/Routes'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Auth0Provider
+    domain="autopartes2001.us.auth0.com"
+    clientId="xb020iwat9uULwYHJjNc0HQ7Bb8PJm5D"
+    redirectUri={window.location.origin}
+  >
+    <Routes />
+  </Auth0Provider>
   );
 }
 
